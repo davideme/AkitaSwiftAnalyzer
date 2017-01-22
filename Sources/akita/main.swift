528b261f62ce510c5b51ff539c7a438e7de54d78
Davide Mendolia <davide@gokarumi.com>
2017-01-22 21:48:28 +0100

    Add basic structure of cli

--- Sources/akita/main.swift
+++ Sources/akita/main.swift
@@ -1,1 +1,24 @@
-print("Hello, world!")
+import Antlr4
+import parser
+import Foundation
+
+func main(arguments: [String]) {
+    guard arguments.count > 1 else {
+        print("at least one document should be provided")
+        return
+    }
+
+    let input = ANTLRFileStream(arguments[1])
+    let lexer = SwiftLexer(input)
+    let stream = CommonTokenStream(lexer)
+    do {
+        let parser = try SwiftParser(stream)
+        let tree = try parser.top_level()
+        let visitor = SwiftVisitor<String>()
+        visitor.visitTop_level(tree)
+    } catch {
+        print(error.localizedDescription)
+    }
+}
+
+main(arguments: CommandLine.arguments)
