524bab0b855b92f53508332e7641c80418d8cf2c
Davide Mendolia <davide@gokarumi.com>
2017-01-31 00:19:30 +0000

    Added Rule

--- Sources/AkitaLib/Diagnostic.swift
+++ Sources/AkitaLib/Diagnostic.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 public struct Diagnostic: Equatable {
+    let descriptor: String
+    let location: String
+    let messageArgs: [String]
 
     public static func ==(lhs: Diagnostic, rhs: Diagnostic) -> Bool {
         return true
--- Sources/AkitaLib/IdentifiersShouldNotContainTypeNames.swift
+++ Sources/AkitaLib/IdentifiersShouldNotContainTypeNames.swift
@@ -9,18 +9,40 @@
 import Foundation
 
 class IdentifiersShouldNotContainTypeNames : DiagnosticAnalyzer {
-    var diagnostic: DiagnosticDescriptor = DiagnosticDescriptor(id: "", title: "", description: "", isEnabledByDefault: true)
 
+    let typesIdentifiers = [
+        "Int8",
+        "UInt8",
+        "Int16",
+        "UInt16",
+        "Int32",
+        "UInt32",
+        "Int64",
+        "UInt64",
+        "Int",
+        "UInt",
+        "Double",
+        "Float",
+        "Bool",
+        "String"
+    ]
+
+    var diagnostic: DiagnosticDescriptor = DiagnosticDescriptor(id: "", title: "", description: "", isEnabledByDefault: true)
     /// Called once at session start to register actions in the analysis context.
     func initialize(context: AnalysisContext) {
-
-        context.registerSymbolAction(action: IdentifiersShouldNotContainTypeNames.AnalyzeNode, syntaxKinds: SyntaxKind.FunctionDeclaration, SyntaxKind.ConstantDeclaration, SyntaxKind.VariableDeclaration)
+        context.registerSymbolAction(action: self.AnalyzeNode, syntaxKinds: SyntaxKind.FunctionDeclaration, SyntaxKind.ConstantDeclaration, SyntaxKind.VariableDeclaration)
     }
 
-    static func AnalyzeNode(context: SyntaxNodeAnalysisContext) {
+    func AnalyzeNode(context:
+        SyntaxNodeAnalysisContext) {
         switch context {
         case let .Function(body, head, name, paramaters):
-            // Test signature
+
+            paramaters.forEach{
+                if self.typesIdentifiers.contains($0.externalName!) {
+                    Diagnostic()
+                }
+            }
             break
 
         default: break
